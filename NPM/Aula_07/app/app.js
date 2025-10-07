6058aad9417d18b7bb5fbb9021e437fed030a17b
Arlenson Lorran <arlensonlorran@gmail.com>
2025-10-07 17:20:17 -0300

    Update app.js

--- NPM/Aula_07/app/app.js
+++ NPM/Aula_07/app/app.js
@@ -2,3 +2,18 @@ const express = require(`express`);
 const app = express();
 const port = 0101;
 
+const indexRouter = require('/routes/index')
+const dataRouter = require('/routes/data')
+const usersRouter = require('/routes/users')
+
+app.use('/', indexRouter)
+app.use('/data', dataRouter)
+app.use('/users', usersRouter)
+
+app.use((req, res) => {
+    res.status(404).send(`<h1>404 - Página não encontrada<h1>`)
+})
+
+app.listen(port, () => {
+    console.log(`Servidor: http:/localhost:${port}`)
+})
