9b6e19b9cf3f331c21444d1fa2dab2623c3fb165
Arlenson Lorran <arlensonlorran@gmail.com>
2025-10-07 21:18:10 -0300

    Update index.js

--- NPM/Aula_07/app/routes/index.js
+++ NPM/Aula_07/app/routes/index.js
@@ -2,18 +2,14 @@ const express = require(`express`);
 const router = express.Router();
 
 const routeName = function(req, res, next) {
-    console.log('Acessando a rota: /');
+    console.log('Acessando a rota: (/)');
     next();
 }
 
 router.use(routeName);
 
 router.get('/', (req, res) => {
-    res.send('<h1>Você esta na rota /<h1>')
+    res.send('<h1>Você esta na rota Raiz</h1>')
 });
-
-router.get('/about', (req, res) => {
-    res.send('<h1>Você esta na rota /about<h1>')
-})
 
 module.exports = router
