a91ba89fde69bfef88350802587102d0b9def109
Arlenson Lorran <arlensonlorran@gmail.com>
2025-10-07 21:17:51 -0300

    Update users.js

--- NPM/Aula_07/app/routes/users.js
+++ NPM/Aula_07/app/routes/users.js
@@ -2,15 +2,31 @@ const express = require('express')
 const router = express.Router()
 
 router.use((req, res, next) => {
-    console.log('Acessando a rota: /users')
+    console.log('Acessando a rota: (/users)')
     next()
 })
 
 router.get('/', (req, res) => {
-    res.send('<h1>Você esta na rota Users')
+    res.send('<h1>Você esta na rota Users</h1>')
 })
 
-router.get('/singup', (req, res) => {
-
+router.get('/signup', (req, res) => {
+    res.send(`<h1>Formulário de Cadastro</h1>
+    <form method="POST" action="/users/signin">
+        <label>Nome de Usuário: <input type="text" name="userID"/></label><br/>
+        <button type="submit">Cadastrar</button>
+    </form>`)
 })
 
+router.post('/signin', (req, res) => {
+    const { userID } = req.body
+    console.log(`Recebido via POST: userID=${userID}`)
+    res.send(`<h1>Bem-vindo, ${userID}!</h1>`)
+})
+
+router.get('/:userID', (req, res) => {
+    const userID = req.params.userID;
+    res.send(`<h1>Você está visualizando o usuário de ID: ${userID}</h1>`)
+})
+
+module.exports = router
