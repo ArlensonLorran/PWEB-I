d6df982eee98c22ac95cd5d24274df0d8e73ccbb
Arlenson Lorran <arlensonlorran@gmail.com>
2025-10-07 21:18:45 -0300

    Update app.js

--- NPM/Aula_07/app/app.js
+++ NPM/Aula_07/app/app.js
@@ -1,19 +1,24 @@
 const express = require(`express`);
 const app = express();
-const port = 0101;
+const port = 3000;
 
-const indexRouter = require('/routes/index')
-const dataRouter = require('/routes/data')
-const usersRouter = require('/routes/users')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const indexRouter = require('./routes/index')
+const aboutRouter = require('./routes/about')
+const dataRouter = require('./routes/data')
+const usersRouter = require('./routes/users')
 
 app.use('/', indexRouter)
 app.use('/data', dataRouter)
 app.use('/users', usersRouter)
+app.use('/about', aboutRouter)
 
 app.use((req, res) => {
-    res.status(404).send(`<h1>404 - Página não encontrada<h1>`)
+    res.status(404).send(`<h1>404 - Página não encontrada</h1>`)
 })
 
 app.listen(port, () => {
-    console.log(`Servidor: http:/localhost:${port}`)
+    console.log(`Servidor: http://localhost:${port}`)
 })
